96a147f82af45eb67f084d7c36fc7c69d3de293a
Marcelo Arias <hello@marceloarias.com>
2026-02-24 11:00:00 -0500

    Fix: Export NegativeSampler from data module

--- src/marcello/data/__init__.py
+++ src/marcello/data/__init__.py
@@ -1,4 +1,5 @@
 from marcello.data.collector import WritingSampleCollector
 from marcello.data.processor import TextProcessor
+from marcello.data.negative_sampler import NegativeSampler
 
-__all__ = ["WritingSampleCollector", "TextProcessor"]
+__all__ = ["WritingSampleCollector", "TextProcessor", "NegativeSampler"]
